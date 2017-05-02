339534afc8ef38841ed57dd27163bb20be974b3f
Bill Fletcher <bill.fletcher@linaro.org>
2017-05-02 15:32:57 +0100

    Removed some redundant print statements

--- iotdm_api.py
+++ iotdm_api.py
@@ -28,20 +28,17 @@ class Iotdm_api():
 	def restConf(self, URI, Cse_name, username, password):
 	    uri = urlparse(URI)
 	    (code, string) = ciotdm.reconf(uri.netloc, Cse_name, (username, password))
-	    # print str(response[0]) + '\n' + response[1]
             return (code, string)
 
 	def cleanup(self, URI, username, password):
 	    uri = urlparse(URI)
 	    (code, string) = ciotdm.kill(uri.netloc, (username, password))
-	    # print str(response[0]) + '\n' + response[1]
             return (code, string)
 
 	def create(self, URI, resource_type, payload, origin=None, requestID=None):
 	    uri = urlparse(URI)
 	    if uri.scheme == "http":
 		(code, string) = ciotdm.create(URI, resource_type, payload, origin, requestID)
-		# print str(response[0]) + '\n' + response[1]
 	    elif uri.scheme == "coap":
 		(code, string) = self.agent.request(self.protocol, "post", URI, payload=payload, ty=resource_type, origin=origin, requestID=requestID)
 	    else:
@@ -53,7 +50,6 @@ class Iotdm_api():
 	    uri = urlparse(URI)
 	    if uri.scheme == "http":
 		(code, string) = ciotdm.retrieve(URI, origin, requestID)
-		# print str(response[0]) + '\n' + response[1]
 	    elif uri.scheme == "coap":
 		(code, string) = self.agent.request(self.protocol, "get", URI, origin=origin, requestID=requestID)
 	    else:
@@ -61,12 +57,10 @@ class Iotdm_api():
 		sys.exit(2)
             return (code, string)
 
-
 	def update(self, URI, payload, origin=None, requestID=None):
 	    uri = urlparse(URI)
 	    if uri.scheme == "http":
 		(code, string) = ciotdm.update(URI, payload, origin, requestID)
-		# print str(response[0]) + '\n' + response[1]
 	    elif uri.scheme == "coap":
 		(code, string) = self.agent.request(self.protocol, "put", URI, payload=payload, origin=origin, requestID=requestID)
 	    else:
@@ -78,7 +72,6 @@ class Iotdm_api():
 	    uri = urlparse(URI)
 	    if uri.scheme == "http":
 		(code, string) = ciotdm.delete(URI, origin, requestID)
-		# print str(response[0]) + '\n' + response[1]
 	    elif uri.scheme == "coap":
 		(code, string) = self.agent.request(self.protocol, "delete", URI, origin=origin, requestID=requestID)
 	    else:
